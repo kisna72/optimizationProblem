0f94ddf18a7f327d896d30420822d10a7f2b4677
Krishna Regmi <kisna72@gmail.com>
2020-04-10 12:13:32 -0500

    add ganttChartFunction

--- src/jobShop.ts
+++ src/jobShop.ts
@@ -56,8 +56,85 @@ class JobShopProblem {
 
     oneDToGanttChart(oned){
         const ganttChartMachineMap:Map<number,number[]> = new Map()
-        this.machines.forEach( (value,key) => {
+        this.machines.forEach((value,key) => {
             ganttChartMachineMap.set(key,[])
+        })
+        const machineIndexTrackingMap:Map<number, number> = new Map() // could do this with array. just easier to read with Map
+        this.jobs.forEach((value, key) => {
+            machineIndexTrackingMap.set(key,0) // start on zero index
+        })
+        const jobEarliestStartMap:Map<number, number> = new Map()
+        this.jobs.forEach((value, key) => {
+            jobEarliestStartMap.set(key, 0)
+        })
+
+        // Helper to add operation 
+        const addOperationToSchedule = (operation:IOperation, jobId:number) => {
+            const scheduleSoFar = ganttChartMachineMap.get(operation.machine)
+            const earliestMachineAvailableTime = scheduleSoFar.length === 0 ? 0 :scheduleSoFar[scheduleSoFar.length-1] + 1
+            const earliestJobStartTime = jobEarliestStartMap.get(jobId);
+            const startTime = Math.max(earliestMachineAvailableTime, earliestJobStartTime)
+            const endTime = startTime + operation.time
+            // console.log("schedule before updating ", scheduleSoFar)
+            if(scheduleSoFar.length === 0){
+                ganttChartMachineMap.set(operation.machine, [jobId, startTime , endTime])
+            } else {
+                ganttChartMachineMap.set(operation.machine, [...scheduleSoFar, jobId, startTime, endTime])
+            }
+
+            return endTime
+            // console.log("schedule after updating",   ganttChartMachineMap.get(operation.machine))
+        }
+        console.log(" ")
+        console.log(" ")
+        console.log(" ")
+        oned.forEach( (jobId, idx, arr) => {
+            // create a schedule for this job.
+            console.log("===========START JOB =================") 
+            console.log("job id value", jobId)
+            const job:IJob = this.jobs.get(jobId);
+            //console.log(job)
+            const operationIndex:number = machineIndexTrackingMap.get(jobId)
+            console.log("operation index is ", operationIndex)
+            const operation = job.operations[operationIndex]
+            console.log("running operation ", operation)
+            
+            // adding to schedule 
+            if(this.isOperationComplex(operation)){
+                const complexOperation = <IComplexOperation>operation // Cast to complex operation type
+                console.log(complexOperation.type, ComplexOperationTypeEnum.CAN_RUN_IN_MULTIPLE_MACINES)
+                if(complexOperation.type === ComplexOperationTypeEnum.CAN_RUN_IN_PARALLEL){
+                    const endTimes = complexOperation.operations.map((operation:IOperation, idx) => {
+                        const endTime = addOperationToSchedule(operation, jobId);
+                        return endTime
+                    })
+                    const maxEndTime = Math.max(...endTimes)
+                    jobEarliestStartMap.set(jobId, maxEndTime + 1);
+
+                } else if(complexOperation.type = ComplexOperationTypeEnum.CAN_RUN_IN_MULTIPLE_MACINES){
+                    console.log("CAN RUN in MULTIPLE MACINES")
+                    // in this case, we just choose one. Future Upgrade, we could just generate multiple gantt chart based on each machine ..
+                    const randomlyChoosenOperationFromMultipleMachineOptions:IComplexOperationUnion = complexOperation.operations[Math.floor(Math.random()*complexOperation.operations.length)]
+                    console.log("randomly choosing a operation ", randomlyChoosenOperationFromMultipleMachineOptions)
+                    const endTime = addOperationToSchedule(<IOperation>randomlyChoosenOperationFromMultipleMachineOptions, jobId); // cast before sending .
+                    jobEarliestStartMap.set(jobId, endTime +1 );
+                } else {
+                    throw new Error("Type not supported yet")
+                }
+            } else {
+                // if operation is not complex, we simply add the operation to the schedule.
+                const _operation = <IOperation>operation
+                const endTime = addOperationToSchedule(_operation, jobId);
+                jobEarliestStartMap.set(jobId, endTime + 1);
+            }
+
+            // at the end, incremebt the index of operation
+            machineIndexTrackingMap.set(jobId, operationIndex+1 )
+
+            console.log(ganttChartMachineMap)
+
+            console.log("++++++++++++++ END JOB +++++++++++++++++++++") 
+
         })
 
 
